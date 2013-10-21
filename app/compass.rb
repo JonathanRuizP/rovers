487bb6b8fb07fc2eb10184343c8f3220f548d810
JonathanRuizP <zinicko_one@hotmail.com>
2013-10-20 19:48:58 -0600

    Text

--- app/compass.rb
+++ app/compass.rb
@@ -2,82 +2,32 @@ class Compass
 	attr_accessor:x
 	attr_accessor:y
 	attr_accessor:orientation
+	
+
 
 	def initialize(x,y,orientation)
 		@x=x
 		@y=y
 		@orientation=orientation
-		
 	end
-
+	
 	def turn_left()
-		hash = {:North => 'N', :South => 'S',
-		:West => 'W', :East => 'E'}		
-		hash[:North]="N"
-		@orientation="W"
-		hash[:North]=@orientation
-		
-	end
-	def turn_leftW()
-		hash = {:North => 'N', :South => 'S',
-		:West => 'W', :East => 'E'}		
-		@orientation="S"
-		hash[:West]=@orientation
-	end
+		orientation ={"N"=>"W","W" => "S", "S" => "E","E" => "N"}
+			@orientation =orientation[@orientation]
 
-	def turn_leftS()
-		hash = {:North => 'N', :South => 'S',
-		:West => 'W', :East => 'E'}		
-		@orientation="E"
-		hash[:South]=@orientation
 	end
+	
+	
+		def turn_right()
+			orientation ={"N" => "E","W" => "N", "S" => "W","E" => "S"} 	 	
+				@orientation =orientation[@orientation]
 
-	def turn_leftE()
-		hash = {:North => 'N', :South => 'S',
-		:West => 'W', :East => 'E'}		
-		@orientation="N"
-		hash[:East]=@orientation
-	end
-	def turn_right()
-		hash = {:North => 'N', :South => 'S',
-		:West => 'W', :East => 'E'}		
-		hash[:North]="N"
-		@orientation="E"
-		hash[:North]=@orientation
-		
-	end
-	def turn_rightW()
-		hash = {:North => 'N', :South => 'S',
-		:West => 'W', :East => 'E'}		
-		@orientation="N"
-		hash[:West]=@orientation
-	end
-
-	def turn_rightS()
-		hash = {:North => 'N', :South => 'S',
-		:West => 'W', :East => 'E'}		
-		@orientation="W"
-		hash[:South]=@orientation
-	end
-
-	def turn_rightE()
-		hash = {:North => 'N', :South => 'S',
-		:West => 'W', :East => 'E'}		
-		@orientation="S"
-		hash[:East]=@orientation
-	end
+	end	
 	def move()
-		hash = {:North => 'N', :South => 'S',
-		:West => 'W', :East => 'E'}	
-		@orientation=="W"
-		hash[:West]=orientation
-		@orientation=="S"
-		hash[:South]=@orientation
-		@orientation=="N"
-		hash[:North]=@orientation
-		@orientation=="E"
-		hash[:East]=@orientation
-	end
+		next_y={"N" => @y+1,"W" => @y, "E" => @y,"S" => @y-1} 	 	
+			@y=next_y[@orientation]
+		next_x={"N" => @x,"W" => @x+1, "E" => @x-1,"S" => @x}
+		 	@x=next_x[@orientation]
 
-
-end 
+	end		
+end
--- spec/compass_spec.rb
+++ spec/compass_spec.rb
@@ -1,34 +1,38 @@
 require_relative "../app/compass"
 
 describe Compass do
-	
+
 	it "should initialize with location" do
-		@compass = Compass.new(1,2,"N")
-		@compass.x.should == 1
-		@compass.y.should == 2
-		@compass.orientation == "N"
-	end
-	it "should turn left from N" do
 		@compass=Compass.new(1,2,"N")
-		@compass.turn_left
-		@compass.orientation.should == "W"
-	end
-	it "should turn left from W" do
-		@compass=Compass.new(1,2,"W")
-		@compass.turn_leftW
-		@compass.orientation.should == "S"
-	end
-	it "should turn left from S"do
-		@compass=Compass.new(1,2,"S")
-		@compass.turn_leftS
-		@compass.orientation.should == "E"
+		@compass.x.should==1
+		@compass.y.should==2
+		@compass.orientation.should=="N"
 	end
 
-	it "should turn left from E"do
-		@compass=Compass.new(1,2,"E")
-		@compass.turn_leftE
-		@compass.orientation.should == "N"
+	it "should turn left from N" do
+		@compass=Compass.new(1,2, "N")
+		@compass.turn_left
+		@compass.orientation.should=="W"
 	end
+
+	it "should turn left from W" do
+		@compass=Compass.new(1,2, "W")
+		@compass.turn_left
+		@compass.orientation.should=="S"
+	end
+
+	it "should turn left from S" do
+		@compass=Compass.new(1,2, "S")
+		@compass.turn_left
+		@compass.orientation.should=="E"
+	end	
+
+	it "should turn left from E" do
+		@compass=Compass.new(1,2, "E")
+		@compass.turn_left
+		@compass.orientation.should=="N"
+	end	
+
 	it "should initialize with location" do
 		@compass = Compass.new(1,2,"N")
 		@compass.x.should == 1
@@ -44,21 +48,22 @@ describe Compass do
 
 	it "should turn right from W" do
 		@compass=Compass.new(1,2,"W")
-		@compass.turn_rightW
+		@compass.turn_right
 		@compass.orientation.should == "N"
 	end
 
 	it "should turn right from S"do
 		@compass=Compass.new(1,2,"S")
-		@compass.turn_rightS
+		@compass.turn_right
 		@compass.orientation.should == "W"
 	end
 
 	it "should turn right from E"do
 		@compass=Compass.new(1,2,"E")
-		@compass.turn_rightE
+		@compass.turn_right
 		@compass.orientation.should == "S"
 	end
+
 	it "should turn north from N" do
 		@compass=Compass.new(1,1,"N")
 		@compass.move
